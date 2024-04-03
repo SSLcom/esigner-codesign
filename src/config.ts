32825070bd8ca335577862dc735343ae155f2652
Mustafa Bayrak <mustafa.bayrak@windowslive.com>
2024-04-03 11:25:17 +0300

    Reformat code

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
 export const CODESIGNTOOL_PROPERTIES =
-    'CLIENT_ID=kaXTRACNijSWsFdRKg_KAfD3fqrBlzMbWs6TwWHwAn8\n' + 'OAUTH2_ENDPOINT=https://login.ssl.com/oauth2/token\n' + 'CSC_API_ENDPOINT=https://cs.ssl.com\n' + 'TSA_URL=http://ts.ssl.com\n' + 'TSA_LEGACY_URL=http://ts.ssl.com/legacy';
+    'CLIENT_ID=kaXTRACNijSWsFdRKg_KAfD3fqrBlzMbWs6TwWHwAn8\n' +
+    'OAUTH2_ENDPOINT=https://login.ssl.com/oauth2/token\n' +
+    'CSC_API_ENDPOINT=https://cs.ssl.com\n' +
+    'TSA_URL=http://ts.ssl.com\n' +
+    'TSA_LEGACY_URL=http://ts.ssl.com/legacy';
 
 export const CODESIGNTOOL_DEMO_PROPERTIES =
     'CLIENT_ID=qOUeZCCzSqgA93acB3LYq6lBNjgZdiOxQc-KayC3UMw\n' +
